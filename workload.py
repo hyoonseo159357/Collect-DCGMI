46ca733ec3f1c5b371314b096e5c59674b49bdcd
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-04 21:31:47 +0900

    Update workload.py

--- workload.py
+++ workload.py
@@ -100,7 +100,7 @@ model.compile(loss=tf.keras.losses.categorical_crossentropy,
 epoch_dict = {}
 class TrainCallback(tf.keras.callbacks.Callback):
     def on_train_begin(self, logs=None):
-        os.system("sudo bash ./dcgmi_field.sh")
+        os.system("sudo bash ./dcgmi_field.sh &")
         os.system("sudo basb ./dstat.sh")
     def on_train_end(self, logs=None):
         os.system("mv dcgmi-log.txt " + file_name )
